ac3f8e91915aee2a759766747642c9b63be0b4f4
DimkaAdam <adamchuk.dima@yahoo.com>
2024-12-09 22:15:15 -0800

    clients updated

--- crm_project/crm/urls.py
+++ crm_project/crm/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .import views
 from .views import ClientCreateAPIView
+from .views import ClientViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'clients', ClientViewSet, basename='client')
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,7 +13,8 @@ urlpatterns = [
     path('deals/', views.deal_list, name='deal_list'),
     path('tasks/', views.task_list, name='task_list'),
     path('pipelines/', views.pipeline_list, name='pipeline_list'),
-    path('api/clients', ClientCreateAPIView.as_view(), name = 'api_add_client'),
+    path('api/clients/', ClientCreateAPIView.as_view(), name='api_add_client'),
+    path('api/', include(router.urls)), # connecting routers to the API
 
 
 ]
--- crm_project/crm/views.py
+++ crm_project/crm/views.py
@@ -3,7 +3,7 @@ from .models import Client, Deals, Task, PipeLine
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from .serializers import ClientSerializer
 
 
@@ -33,3 +33,7 @@ class ClientCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
